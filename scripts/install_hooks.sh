2697fc206ddf7522a19777328d1475e291c62182
Thomas Leibovici <thomas.leibovici@cea.fr>
2014-12-18 12:00:47 +0100

    Git hooks scripts and installer.
    Running ./scripts/install_hooks.sh installs:
    	- automatic patch check at commit
    	- insertion of Change-Id to commit message
    
    Change-Id: Ibc1587043ddb013f11e5932646a75e97047d5de7

---
+++ scripts/install_hooks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+script_dir=$(dirname $(readlink -m $0))
+hook_dir=$(readlink -m $script_dir/../.git/hooks)
+
+if [ ! -e $hook_dir/commit-msg ]; then
+    echo "$hook_dir/commit-msg -> ../../scripts/commit-msg"
+    ln -s "../../scripts/commit-msg" $hook_dir/commit-msg
+fi
+
+if [ ! -x $script_dir/commit-msg ]; then
+    echo "chmod u+x $script_dir/commit-msg"
+    chmod u+x $script_dir/commit-msg
+fi
+
+if [ ! -e $hook_dir/pre-commit ]; then
+    echo "$hook_dir/pre-commit -> ../../scripts/pre-commit"
+    ln -s "../../scripts/pre-commit" $hook_dir/pre-commit
+fi
+
+if [ ! -x $script_dir/pre-commit ]; then
+    echo "chmod u+x $script_dir/pre-commit"
+    chmod u+x $script_dir/pre-commit
+fi
