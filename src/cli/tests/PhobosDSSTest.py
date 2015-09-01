23d9d8aa4f58f0ac531e170167996f9455b06604
Henri Doreau <henri.doreau@cea.fr>
2015-09-01 14:45:37 +0200

    cli: Add test for device_get
    Add test to make sure filtering works as expected.
    
    Change-Id: I2da44bb76b6199df9b9e2907e6c2c65003641a88

--- src/cli/tests/PhobosDSSTest.py
+++ src/cli/tests/PhobosDSSTest.py
@@ -29,5 +29,14 @@ class DSSClientTest(unittest.TestCase):
         self.assertRaises(DSSError, cli.connect, inval0=0, inval1=1)
         self.assertRaises(DSSError, cli.connect)
 
+    def test_list_devices_by_family(self):
+        """List devices family by family."""
+        cli = Client()
+        cli.connect(dbname='phobos', user='phobos', password='phobos')
+        for fam in ('tape', 'disk', 'dir'):
+            for x in cli.device_get(family=fam):
+                self.assertEqual(x.family, fam)
+        cli.disconnect()
+
 if __name__ == '__main__':
     unittest.main()
