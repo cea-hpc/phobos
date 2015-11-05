03d6777d52ae8ef113de3dabd1662acd09af6e8b
Henri Doreau <henri.doreau@cea.fr>
2015-11-05 11:29:17 +0100

    tests: reset DB before running CLI tests
    Take test lock and clean DB before running CLI tests.
    
    Change-Id: Iaf6260e16bb91e177e29e78eef39f74004409063

--- src/cli/tests/run_tests.sh
+++ src/cli/tests/run_tests.sh
@@ -13,6 +13,10 @@ export LD_LIBRARY_PATH=$PHO_STORELIB_PATH:$PHO_PYTHON_PATH
 export PYTHONPATH="$PHO_PYTHON_PATH"
 
 
+. ../../tests/setup_db.sh
+drop_tables
+setup_tables
+
 for test_case in *Test.py
 do
     $PY $test_case || exit 1
