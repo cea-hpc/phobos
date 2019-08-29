b6e3351531926fa253b2d26672555c96ad6ef639
Patrice LUCAS <patrice.lucas@cea.fr>
2019-08-29 15:10:28 +0200

    ci : add ci directory
    This directory initially contains the two following scripts :
    -setup_quadstor.sh ,
    -run-ci.sh .
    
    Change-Id: Ife6c1843ed35ce07ae17121ca2a2ad7fb5296101
    Signed-off-by: Patrice LUCAS <patrice.lucas@cea.fr>
    Reviewed-on: https://cws-fleury.labs.ocre.cea.fr/gerrit/5015
    Reviewed-by: Linter
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Florent Monjalet <florent.monjalet@cea.fr>
    Reviewed-by: Thomas Leibovici <thomas.leibovici@cea.fr>

---
+++ ci/run-ci.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+# vim:expandtab:shiftwidth=4:tabstop=4:
+
+# This scripts configure/compile/run phobos tests.
+
+# (c) 2014-2019 CEA/DAM
+# Licensed under the terms of the GNU Lesser GPL License version 2.1
+
+set -xe
+
+#set phobos root as cwd from phobos/ci directory
+cur_dir=$(dirname $(readlink -m $0))
+cd "$cur_dir"/..
+
+# export PKG_CONFIG_PATH=/usr/pgsql-9.4/lib/pkgconfig;
+./autogen.sh;
+./configure;
+make rpm;
+
+make clean || cat src/tests/test-suite.log;
+make
+# FIXME: when cloning the repo, some scripts do not have o+rx
+# permissions, it is however necessary to execute them as postgres
+chmod o+rx . .. ./scripts/phobos_db{,_local}
+sudo -u postgres ./scripts/phobos_db_local drop_db || true
+sudo -u postgres ./scripts/phobos_db_local setup_db -s -p phobos
+export VERBOSE=1
+sudo -E make check
---
+++ ci/setup_quadstor.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+# vim:expandtab:shiftwidth=4:tabstop=4:
+
+# This scripts relies on quadstor to setup a virtual tape library needed by
+# phobos CI tests.
+# (This script was tested with quadstor-vtl-ext-3.0.28.)
+
+# (c) 2014-2019 CEA/DAM
+# Licensed under the terms of the GNU Lesser GPL License version 2.1
+
+set -xe
+
+# Create a loopback device for storage
+VTL_STORAGE=/tmp/vtl_storage
+dd if=/dev/zero of=$VTL_STORAGE bs=1M count=5000
+losetup /dev/loop0 $VTL_STORAGE
+mdadm --create /dev/md0 --level=linear --force --raid-devices=1 /dev/loop0
+systemctl restart quadstorvtl
+
+# Create a pool with no dedup (dedup needs at least 11G disk space)
+/quadstorvtl/bin/spconfig -a -g phobos-pool
+
+# Give storage device to quadstorvtl
+/quadstorvtl/bin/bdconfig -a -d /dev/md0 -g phobos-pool
+
+# VTL type:
+# 09      IBM System Storage TS3100
+# Drive type:
+# 20      IBM 3580 Ultrium5
+# Note: --drive-{type,count} are repeatable
+# TODO: add LTO8 drives as well
+/quadstorvtl/bin/vtconfig --add --vtl=phobos-ibm \
+    --type=09 --slots=20 --ieports=4 --drive-type=20 --drive-count=4
+
+# Add 20 cartridges (tapes), type:
+# 12      LTO 5 1500GB
+# TODO: add LTO8 tapes as well
+/quadstorvtl/bin/vcconfig --add --vtl=phobos-ibm \
+    --type 12 --pool=phobos-pool --label=P00000 --count=10
+
+# Symlink for phobos tests
+ln -s /dev/sg5 /dev/changer
