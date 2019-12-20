a327ed60adb664c75bc4d78dee23f2626e8990c6
Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
2019-12-20 10:30:19 +0100

    ci: add a new script which installs phobos RPM on the VM
    The script configures the virtual machine to be phobos ready-to-use.
    
    Change-Id: Id4f154b21a9d6890ae18e1d413f40e0da7f5f7ad
    Signed-off-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-on: https://cws-fleury.labs.ocre.cea.fr/gerrit/6484
    Reviewed-by: Linter
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>
    Reviewed-by: Thomas Leibovici <thomas.leibovici@cea.fr>

---
+++ ci/install-ci.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+# vim:expandtab:shiftwidth=4:tabstop=4:
+
+# This script compiles and installs the phobos RPM and configure a ready-to-use
+# environment (for demo or prod tests)
+
+# (c) 2014-2019 CEA/DAM
+# Licensed under the terms of the GNU Lesser GPL License version 2.1
+
+set -xe
+
+# set phobos root as cwd from phobos/ci directory
+cur_dir=$(dirname $(readlink -m $0))
+cd "$cur_dir"/..
+
+# compile phobos RPM
+./autogen.sh
+./configure
+make rpm
+
+# (re-)install RPM
+if [ ! -z `type -t phobos` ]; then
+    sudo yum -y remove phobos
+fi
+sudo yum -y install rpms/RPMS/x86_64/phobos-1*
+
+# clean phobos compilation directory
+cd ..
+rm -rf phobos
+
+# setup phobos database
+sudo -u postgres phobos_db drop_db || true
+sudo -u postgres phobos_db setup_db -s -p phobos
+
+# create socket directory (in case we use the daemon in interactive mode)
+if [ ! -d "/run/phobosd" ]; then
+    sudo mkdir /run/phobosd
+fi
