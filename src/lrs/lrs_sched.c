46d2f1f837bc0859e49f90a83ab76060e40d7dce
Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
2021-01-25 12:39:37 +0100

    fix: add a 'fallthrough' comment on a no-break switch case
    Retrieved by coverity: this error is marked as "Missing break in
    switch".
    
    Change-Id: I239218b522e6ccd2320a4eac02bf23a1af92a600
    Signed-off-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-on: https://gerrit.ccc.ocre.cea.fr/gerrit/c/phobos/+/8201
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>

--- src/lrs/lrs_sched.c
+++ src/lrs/lrs_sched.c
@@ -2048,6 +2048,7 @@ static int sched_media_prepare(struct lrs_sched *sched,
         if (!med->flags.get)
             LOG_RETURN(-EPERM, "Cannot do a get, get flag is false on '%s'",
                        id->name);
+        /* fall through */
     case LRS_OP_WRITE:
         if (med->fs.status == PHO_FS_STATUS_BLANK)
             LOG_RETURN(-EINVAL, "Cannot do I/O on unformatted media '%s'",
