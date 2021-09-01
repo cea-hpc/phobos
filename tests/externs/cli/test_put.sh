28e67f37f07aafcf59c8483d3299f5541d5dc6a0
Yoann Valeri <yoann.valeri@cea.fr>
2021-09-01 13:36:14 +0200

    tests: merge test_overwrite into test_put
    Change-Id: Ic9a866c32b2c0aae7f1780f38c0537ea46d1fdbe
    Signed-off-by: Yoann Valeri <yoann.valeri@cea.fr>
    Reviewed-on: https://gerrit.ccc.ocre.cea.fr/gerrit/c/phobos/+/9174
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

--- tests/externs/cli/test_put.sh
+++ tests/externs/cli/test_put.sh
@@ -31,6 +31,11 @@ test_dir=$(dirname $(readlink -e $0))
 . $test_dir/../../test_env.sh
 . $test_dir/../../setup_db.sh
 . $test_dir/../../test_launch_daemon.sh
+. $test_dir/../../tape_drive.sh
+
+################################################################################
+#                                    SETUP                                     #
+################################################################################
 
 function dir_setup
 {
@@ -46,9 +51,26 @@ function dir_setup
 function cleanup
 {
     waive_daemon
-    drop_tables
     rm -rf $dirs
+    drop_tables
+
+    if [[ -w /dev/changer ]]; then
+        drain_all_drives
+    fi
 }
+
+export PHOBOS_STORE_default_family="dir"
+
+drop_tables
+setup_tables
+invoke_daemon
+dir_setup
+
+trap cleanup ERR EXIT
+
+################################################################################
+#                               SIMPLE PUT TESTS                               #
+################################################################################
 
 function test_extent_path
 {
@@ -91,6 +113,12 @@ function test_extent_path
         error "oid2 extent path (pt2) should contain object version"
 }
 
+test_extent_path
+
+################################################################################
+#                         PUT WITH --LYT-PARAMS OPTION                         #
+################################################################################
+
 function lyt_params_helper
 {
     $phobos put --family dir $2 /etc/hosts $1
@@ -112,9 +140,173 @@ function test_lyt_params
     lyt_params_helper "lp5" "--layout raid1 --lyt-params repl_count=1" 1
 }
 
-setup_tables
-invoke_daemon
-trap cleanup EXIT
-dir_setup
-test_extent_path
 test_lyt_params
+
+################################################################################
+#                         PUT WITH --OVERWRITE OPTION                          #
+################################################################################
+
+function output_checkout
+{
+    # $1 = type of item to list
+    # $2 = output to show
+    # $3 = oid to show
+    # $4 = expected result of the output
+    # $5 = check deprecated objects or not
+    # The parameter $5 is only used when checking the version numbers.
+    # Otherwise it isn't specified, i.e. empty.
+    local result=$($phobos $1 list $5 --output $2 $3)
+
+    if [ "$result" != "$4" ]; then
+        error "$1 $3 should have $2 : $4"
+    fi
+}
+
+function put_checkout
+{
+    if [ "$2" == *"--overwrite"* ]; then
+        local action="overwritten"
+    else
+        local action="created"
+    fi
+
+    $phobos put $2 /etc/hosts $1 ||
+        error "Object $1 should have been $action : " \
+              "phobos put $2 /etc/hosts $1"
+}
+
+function test_overwrite_and_delete
+{
+    local output_func="output_checkout object version oid3"
+    local put_func="put_checkout oid3"
+
+    echo "**** INSERTING AND OVERWRITING OBJECT OID3 ****"
+    $put_func
+    $put_func "--overwrite"
+
+    $output_func "1" "--deprecated"
+    $output_func "2"
+
+    echo "**** DELETING OBJECT OID3 ****"
+    $phobos delete oid3 || error "Object oid3 should be deleted"
+
+    local expect=$(printf "1\n2")
+    $output_func "$expect" "--deprecated"
+}
+
+function test_double_overwrite
+{
+    local output_func="output_checkout object version oid4"
+    local put_func="put_checkout oid4"
+
+    echo "**** INSERTING BY OVERWRITING OBJECT OID4 AND DOUBLE OVERWRITE ****"
+    $put_func "--overwrite"
+
+    $output_func "1"
+
+    $put_func "--overwrite"
+
+    $output_func "1" "--deprecated"
+    $output_func "2"
+
+    $put_func "--overwrite"
+
+    local expect=$(printf "1\n2")
+    $output_func "$expect" "--deprecated"
+    $output_func "3"
+}
+
+function test_overwrite_metadata
+{
+    local output_func="output_checkout object user_md oid5"
+    local put_func="put_checkout oid5"
+
+    echo "**** INSERTING OBJECT OID5 AND OVERWRITING METADATA ****"
+    $put_func "--metadata a=b"
+
+    $output_func '{"a": "b"}'
+
+    $put_func "--overwrite"
+
+    $output_func '{"a": "b"}' "--deprecated"
+    $output_func '{"a": "b"}'
+
+    $put_func "--metadata c=d --overwrite"
+
+    local expect=$(printf '{"a": "b"}\n{"a": "b"}')
+    $output_func "$expect" "--deprecated"
+    $output_func '{"c": "d"}'
+}
+
+function test_overwrite_lyt_params
+{
+    local output_func="output_checkout extent ext_count oid6"
+    local put_func="put_checkout oid6"
+
+    echo "**** INSERTING OBJECT OID6 AND OVERWRITING LAYOUT ****"
+    $put_func "--lyt-params repl_count=1"
+
+    $output_func "1"
+
+    $put_func "--lyt-params repl_count=1 --overwrite"
+
+    local expect=$(printf "1\n1")
+    $output_func "$expect"
+
+    $put_func "--lyt-params repl_count=2 --overwrite"
+
+    local expect=$(printf "1\n1\n2")
+    $output_func "$expect"
+
+    $put_func "--lyt-params repl_count=1 --overwrite"
+
+    local expect=$(printf "1\n1\n2\n1")
+    $output_func "$expect"
+}
+
+function test_overwrite_family
+{
+    drain_all_drives
+
+    $phobos drive add --unlock /dev/st0 ||
+        error "Drive /dev/st0 should have been added"
+
+    $phobos tape add -t lto5 P00000L5 ||
+        error "Tape P00000L5 should have been added"
+    $phobos tape format --unlock P00000L5 ||
+        error "Tape P00000L5 should have been formated"
+
+    local output_func="output_checkout extent family oid7"
+    local put_func="put_checkout oid7"
+
+    echo "**** INSERTING OBJECT OID7 AND OVERWRITING FAMILY ****"
+    $put_func
+
+    $output_func "['dir']"
+
+    $put_func "--overwrite"
+
+    local expect=$(printf "['dir']\n['dir']")
+    $output_func "$expect"
+
+    $put_func "--family tape --overwrite"
+
+    local expect=$(printf "['dir']\n['dir']\n['tape']")
+    $output_func "$expect"
+
+    $put_func "--overwrite"
+
+    local expect=$(printf "['dir']\n['dir']\n['tape']\n['dir']")
+    $output_func "$expect"
+}
+
+test_overwrite_and_delete
+test_double_overwrite
+test_overwrite_metadata
+test_overwrite_lyt_params
+
+# Tape tests are available only if /dev/changer exists, which is the entry
+# point for the tape library.
+if [[ -w /dev/changer ]]; then
+    test_overwrite_family
+fi
