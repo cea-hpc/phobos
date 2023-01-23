0d15d8cabe484f60a7818ccd55f81d80c2ac15fa
Patrice LUCAS <patrice.lucas@cea.fr>
2023-01-23 10:38:52 +0100

    tape_certif: licence headers
    Change-Id: Ifb032da8a10eec0f1599a8d9976ea95c52bfe2f0
    Signed-off-by: Patrice LUCAS <patrice.lucas@cea.fr>
    Reviewed-on: https://gerrit.ccc.ocre.cea.fr/gerrit/c/phobos/+/11162
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- tape_library_certification/phobos_tape_library_test.bash
+++ tape_library_certification/phobos_tape_library_test.bash
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+#  All rights reserved (c) 2014-2023 CEA/DAM.
+#
+#  This file is part of Phobos.
+#
+#  Phobos is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 2.1 of the License, or
+#  (at your option) any later version.
+#
+#  Phobos is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Phobos. If not, see <http://www.gnu.org/licenses/>.
 
 if hash phobos_tape_library_test >>/dev/null 2>&1 ; then
     binary_test="phobos_tape_library_test"
