5a205986505a4281769461a55f865c1daba3fe83
Augustin BOULARD <augustin.boulard.ocre@cea.fr>
2023-09-05 10:58:54 +0200

    tests: add functions for layout tests
    This patch adds a function to generate an ext4 file system. This
    function takes the size of the filesystem as a parameter. The goal is
    to easily setup phobos directories with a small size to force and
    control the creation of splits.
    
    Change-Id: I8b171c710a31a370b481b21e53cd147416e45bb1
    Signed-off-by: Augustin BOULARD <augustin.boulard.ocre@cea.fr>
    Signed-off-by: Courrier Guillaume <guillaume.courrier@cea.fr>
    Reviewed-on: https://gerrit.ccc.ocre.cea.fr/gerrit/c/phobos/+/12047
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Yoann VALERI <yoann.valeri@cea.fr>
    Reviewed-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- tests/utils_generation.sh
+++ tests/utils_generation.sh
@@ -39,3 +39,43 @@ function generate_prefix_id
 {
     echo "$(basename -s .sh ${BASH_SOURCE[1]})/${FUNCNAME[1]}"
 }
+
+function make_tmp_fs()
+{
+    local mount_point=$(mktemp -d)
+    local file_path=$(mktemp)
+    local loop_device
+    local unit=${1: -1} # Last char
+    local count=${1::-1} # All the string except the last char
+
+    [[ $unit =~ [kKmMgG] ]] ||
+        error "Invalid unit $unit"
+
+    if (( count < 60)) && [[ $unit == k || $unit == K ]]; then
+        error "Ext4 size should be at least 60K"
+    fi
+
+    dd if=/dev/zero of=$file_path count=$count bs=1$unit 2>/dev/null
+
+    loop_device=$(losetup -f)
+    losetup $loop_device $file_path
+
+    mkfs.ext4 $loop_device >/dev/null 2>&1
+
+    mkdir -p $mount_point
+    mount $loop_device $mount_point
+
+    echo $mount_point
+}
+
+cleanup_tmp_fs()
+{
+    local mount_point=$1
+    local loop_device=$(df $mount_point | tail -n 1 | awk '{print $1}')
+    local back_file=$(losetup -l -O BACK-FILE $loop_device | tail -n 1)
+
+    umount $mount_point
+    rmdir $mount_point
+    losetup -d $loop_device
+    rm $back_file
+}
