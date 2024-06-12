61e18232768c1561b5d5ee4b2fe4302d0184f070
Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
2024-06-12 15:23:42 +0200

    tests: add test for access and creation times
    Change-Id: I639f34dc70d9edadd3fe3371645ec6526a76f73a
    Signed-off-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-on: https://gerrit.ccc.ocre.cea.fr/gerrit/c/phobos/+/13200
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>
    Reviewed-by: Yoann VALERI <yoann.valeri@cea.fr>

--- tests/externs/cli/test_get.sh
+++ tests/externs/cli/test_get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-#  All rights reserved (c) 2014-2022 CEA/DAM.
+#  All rights reserved (c) 2014-2024 CEA/DAM.
 #
 #  This file is part of Phobos.
 #
@@ -83,6 +83,120 @@ function test_get
     rm /tmp/out
 }
 
+function test_creation_and_access_times
+{
+    $phobos put --family dir /etc/hosts oid-time
+    $phobos put --family dir --overwrite /etc/hosts oid-time
+    $phobos put --family dir --overwrite /etc/hosts oid-time
+
+    local uuid=$($phobos object list --output uuid oid-time)
+
+    local ctm3=$($phobos object list --output creation_time oid-time)
+    ctm3=$(date -d "$ctm3" +"%s")
+    local ctm2=$($phobos object list --format csv --deprecated --output \
+                 version,creation_time oid-time | grep "2," | cut -d',' -f2)
+    ctm2=$(date -d "$ctm2" +"%s")
+    local ctm1=$($phobos object list --format csv --deprecated --output \
+                 version,creation_time oid-time | grep "1," | cut -d',' -f2)
+    ctm1=$(date -d "$ctm1" +"%s")
+
+    local atm3=$($phobos object list --output access_time oid-time)
+    atm3=$(date -d "$atm3" +"%s")
+    local atm2=$($phobos object list --format csv --deprecated --output \
+                 version,access_time oid-time | grep "2," | cut -d',' -f2)
+    atm2=$(date -d "$atm2" +"%s")
+    local atm1=$($phobos object list --format csv --deprecated --output \
+                 version,access_time oid-time | grep "1," | cut -d',' -f2)
+    atm1=$(date -d "$atm1" +"%s")
+
+    [ $ctm1 -le $ctm2 ] \
+        || error "Creation time of 1st version is greater than 2nd version one"
+    [ $ctm2 -le $ctm3 ] \
+        || error "Creation time of 2nd version is greater than 3rd version one"
+    [ $atm1 -le $atm2 ] \
+        || error "Access time of 1st version is greater than 2nd version one"
+    [ $atm2 -le $atm3 ] \
+        || error "Access time of 2nd version is greater than 3rd version one"
+
+    sleep 1
+    $valg_phobos get --uuid $uuid --version 1 oid-time /tmp/out
+    rm /tmp/out
+
+    local atm=$($phobos object list --format csv --deprecated --output \
+                version,access_time oid-time | grep "1," | cut -d',' -f2)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm1 -lt $atm ] \
+        || error "Access time of 1st version was not updated"
+    atm1=$atm
+
+    atm=$($phobos object list --format csv --deprecated --output \
+          version,access_time oid-time | grep "2," | cut -d',' -f2)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm2 -eq $atm ] \
+        || error "Access time of 2nd version was updated, only 1st should be"
+
+    atm=$($phobos object list --output access_time oid-time)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm3 -eq $atm ] \
+        || error "Access time of 3rd version was updated, only 1st should be"
+
+    $valg_phobos get oid-time /tmp/out
+    rm /tmp/out
+
+    atm=$($phobos object list --format csv --deprecated --output \
+                version,access_time oid-time | grep "1," | cut -d',' -f2)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm1 -eq $atm ] \
+        || error "Access time of 1st version was updated, only 3rd should be"
+
+    atm=$($phobos object list --format csv --deprecated --output \
+          version,access_time oid-time | grep "2," | cut -d',' -f2)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm2 -eq $atm ] \
+        || error "Access time of 2nd version was updated, only 3rd should be"
+
+    atm=$($phobos object list --output access_time oid-time)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm3 -lt $atm ] \
+        || error "Access time of 3rd version was not updated"
+    atm3=$atm
+
+    $valg_phobos get --uuid $uuid --version 2 oid-time /tmp/out
+    rm /tmp/out
+
+    atm=$($phobos object list --format csv --deprecated --output \
+                version,access_time oid-time | grep "1," | cut -d',' -f2)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm1 -eq $atm ] \
+        || error "Access time of 1st version was updated, only 2nd should be"
+
+    atm=$($phobos object list --format csv --deprecated --output \
+          version,access_time oid-time | grep "2," | cut -d',' -f2)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm2 -lt $atm ] \
+        || error "Access time of 2nd version was not updated"
+    atm2=$atm
+
+    atm=$($phobos object list --output access_time oid-time)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm3 -eq $atm ] \
+        || error "Access time of 3rd version was updated, only 2nd should be"
+
+    sleep 1
+    $phobos del oid-time
+    atm=$($phobos object list --format csv --deprecated --output \
+          version,access_time oid-time | grep "3," | cut -d',' -f2)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm3 -eq $atm ] \
+        || error "Access time should not be updated on delete operation"
+
+    $phobos undel oid oid-time
+    atm=$($phobos object list --output access_time oid-time)
+    atm=$(date -d "$atm" +"%s")
+    [ $atm3 -eq $atm ] \
+        || error "Access time should not be updated on undelete operation"
+}
+
 function test_errors
 {
     $valg_phobos get oid2 /tmp/out \
@@ -110,4 +224,5 @@ trap cleanup EXIT
 setup
 
 test_get
+test_creation_and_access_times
 test_errors
