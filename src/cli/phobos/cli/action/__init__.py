ace91c9ca34dda8649274d5b4727613dbde0c957
Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
2024-12-11 10:24:18 +0100

    cli: add Copy feature actions
    Those actions are the ones defined for the copy features. They will be
    used in the following patches.
    
    The goal will be, in future developments, to enrich and/or use them in
    CLI handlers.
    
    Change-Id: I0acac359bbb9069ec9a62c54e59d31c27f55efdb
    Signed-off-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
    Reviewed-on: https://gerrit.ccc.ocre.cea.fr/gerrit/c/phobos/+/14100
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Gauthier Evraerd <gauthier.evraerd@cea.fr>
    Reviewed-by: Yoann VALERI <yoann.valeri@cea.fr>
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>

---
+++ src/cli/phobos/cli/action/__init__.py
@@ -0,0 +1,33 @@
+#
+#  All rights reserved (c) 2014-2024 CEA/DAM.
+#
+#  This file is part of Phobos.
+#
+#  Phobos is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 2.1 of the License, or
+#  (at your option) any later version.
+#
+#  Phobos is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Phobos. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Utilities for Phobos CLI actions
+"""
+
+from phobos.cli.common import BaseOptHandler
+
+class ActionOptHandler(BaseOptHandler):
+    """Option handler for actions"""
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
---
+++ src/cli/phobos/cli/action/list.py
@@ -0,0 +1,34 @@
+#
+#  All rights reserved (c) 2014-2024 CEA/DAM.
+#
+#  This file is part of Phobos.
+#
+#  Phobos is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 2.1 of the License, or
+#  (at your option) any later version.
+#
+#  Phobos is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Phobos. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+List action for Phobos CLI
+"""
+
+from phobos.cli.action import ActionOptHandler
+
+class ListOptHandler(ActionOptHandler):
+    """Option handler for list action"""
+    label = 'list'
+    descr = 'list resources'
+
+    @classmethod
+    def add_options(cls, parser):
+        super(ListOptHandler, cls).add_options(parser)
+        parser.add_argument('rsc', help='resource to list')
