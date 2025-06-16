2c0f90ebff3676f63085a50cf0543b90e4ddded0
VALERI Yoann <yoann.valeri@cea.fr>
2025-06-16 14:51:53 +0200

    common: handle external command errors better
    This patch changes the external command error translation function to
    instead use the standard `spawn_check_wait_status` function of the
    glibc, or `spawn_check_exit_status` for older versions, instead of
    `child_status2errno`, which would only handle a few cases, and not
    particularly well either.
    
    Change-Id: I94162eb19a308dfc49d8c867a03fa6d4641e4d11
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-on: https://gerrit.ccc.ocre.cea.fr/gerrit/c/phobos/+/14829
    Tested-by: Jenkins s8open_nr <s8open_nr@ccc.ocre.cea.fr>
    Reviewed-by: Gauthier EVRAERD <gauthier.evraerd@cea.fr>
    Reviewed-by: Patrice LUCAS <patrice.lucas@cea.fr>

--- src/common/common.c
+++ src/common/common.c
@@ -87,41 +87,9 @@ static inline void ctx_decref(struct exec_ctx *ctx)
         g_main_loop_quit(ctx->loop);
 }
 
-/**
- * Convert a subprocess return value into a human-readable message and a
- * meaningful errno code for proper error logging and escalation to upper
- * layers.
- */
-static int child_status2errno(int status, const char **msg)
-{
-    if (WIFEXITED(status)) {
-        switch (WEXITSTATUS(status)) {
-        case 0:
-            *msg = "no error";
-            return 0;
-        case 126:
-            *msg = "permissions problem or command is not an executable";
-            return -EPERM;
-        case 127:
-            *msg = "command not found";
-            return -ENOENT;
-        case 128:
-            *msg = "invalid argument to exit";
-            return -EINVAL;
-        default:
-            *msg = "external command exited";
-            return -ECHILD;
-        }
-    }
-
-    if (WIFSIGNALED(status)) {
-        *msg = "command terminated by signal";
-        return -EINTR;
-    }
-
-    *msg = "unexpected error";
-    return -EIO;
-}
+#ifndef g_spawn_check_wait_status
+#define g_spawn_check_wait_status g_spawn_check_exit_status
+#endif
 
 /**
  * External process termination handler.
@@ -129,14 +97,18 @@ static int child_status2errno(int status, const char **msg)
 static void watch_child_cb(GPid pid, gint status, gpointer data)
 {
     struct exec_ctx *ctx = data;
-    const char      *err = "";
-    ENTRY;
+    const char *error_string;
 
     pho_debug("Child %d terminated with %d", pid, status);
 
     if (status != 0) {
-        ctx->rc = child_status2errno(status, &err);
-        pho_error(ctx->rc, "Command failed: %s", err);
+        GError *error = NULL;
+
+        g_spawn_check_wait_status(status, &error);
+        error_string = error->message;
+        ctx->rc = error->code;
+
+        pho_debug("External command failed: %s", error_string);
     }
 
     g_spawn_close_pid(pid);
@@ -165,13 +137,31 @@ static gboolean readline_cb(GIOChannel *channel, GIOCondition cond, gpointer ud)
     }
 
     res = g_io_channel_read_line(channel, &line, &size, NULL, &error);
-    if (res != G_IO_STATUS_NORMAL) {
-        pho_error(EIO, "Cannot read from child: %s", error->message);
+    switch (res) {
+    case G_IO_STATUS_EOF:
+        g_io_channel_shutdown(channel, true, &error);
         g_error_free(error);
-    } else {
+        g_io_channel_unref(channel);
+        ctx_decref(args->exec_ctx);
+
+        return false;
+    case G_IO_STATUS_ERROR:
+        if (error) {
+            pho_error(EIO, "Cannot read from child: %s", error->message);
+            g_error_free(error);
+        } else {
+            pho_error(0, "Cannot read from child, but no error was given");
+        }
+
+        return false;
+    case G_IO_STATUS_NORMAL:
         args->cb(args->udata, line, size, args->ident);
         g_free(line);
+    case G_IO_STATUS_AGAIN:
+        return true;
     }
+
+     __builtin_unreachable();
     return true;
 }
 
@@ -181,18 +171,18 @@ static gboolean readline_cb(GIOChannel *channel, GIOCondition cond, gpointer ud)
  */
 int command_call(const char *cmd_line, parse_cb_t cb_func, void *cb_arg)
 {
-    struct exec_ctx   ctx = { 0 };
-    GPid              pid;
-    gint              ac;
-    gchar           **av = NULL;
-    GError           *err_desc = NULL;
-    GSpawnFlags       flags = G_SPAWN_SEARCH_PATH | G_SPAWN_DO_NOT_REAP_CHILD;
-    GIOChannel       *out_chan;
-    GIOChannel       *err_chan;
-    int               p_stdout;
-    int               p_stderr;
-    bool              success;
-    int               rc = 0;
+    GSpawnFlags flags = G_SPAWN_SEARCH_PATH | G_SPAWN_DO_NOT_REAP_CHILD;
+    struct exec_ctx ctx = { 0 };
+    GError *err_desc = NULL;
+    GIOChannel *out_chan;
+    GIOChannel *err_chan;
+    gchar **av = NULL;
+    int p_stdout;
+    int p_stderr;
+    bool success;
+    int rc = 0;
+    GPid pid;
+    gint ac;
 
     success = g_shell_parse_argv(cmd_line, &ac, &av, &err_desc);
     if (!success)
@@ -200,8 +190,8 @@ int command_call(const char *cmd_line, parse_cb_t cb_func, void *cb_arg)
                  cmd_line, err_desc->message);
 
     ctx.loop = g_main_loop_new(NULL, false);
-    ctx.ref  = 0;
-    ctx.rc   = 0;
+    ctx.ref = 0;
+    ctx.rc = 0;
 
     pho_debug("Spawning external command '%s'", cmd_line);
 
